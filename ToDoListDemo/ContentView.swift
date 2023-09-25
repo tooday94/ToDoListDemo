f975b0860cd9ba9175c7d448c0830827a2fc2a81
tooday94 <masharipovbakhrom@gmail.com>
2023-09-25 23:36:46 +0500

    Initial Commit

---
+++ ToDoListDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListDemo
+//
+//  Created by Bakhrom Masharipov on 25.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Bakhrom Masharipov on 25.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
